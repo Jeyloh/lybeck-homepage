bbd520685a3cd90e3a3b5d2e81da1931550d10dc
jorgen.lybeck.hansen@schibsted.com <jorgen.lybeck.hansen@schibsted.com>
2023-10-20 14:40:58 +0200

    initial commit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,17 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+       <Script strategy='beforeInteractive' type="text/javascript" src="/fluid-animation/dat.gui.min.js" />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="mobile-web-app-capable" content="yes" />
+      </Head>
       <body>
         <Main />
         <NextScript />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,129 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import { useEffect, useState } from 'react'
+
+declare global {
+  interface Window {
+    updateFluidConfig: (config: Config, guiStarted: boolean) => void;
+    invokeSplash: () => void;
+    startGUI: () => void;
+    fluidConfig: {
+      SPLAT_RADIUS: number;
+      SPLAT_FORCE: number;
+    }
+  }
+}
+interface Config {
+  SPLAT_RADIUS?: number
+  SPLAT_FORCE?: number
+  DENSITY_DISSIPATION?: number
+}
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
+  useEffect(() => {
+    const existingScript = document.getElementById("fluid-animation-script");
+    if (existingScript) return;
+    const script = document.createElement("script");
+    script.id = "fluid-animation-script"
+    script.src = "/fluid-animation/script.js"
+    script.type = "text/javascript"
+    document.body.appendChild(script)
+  }, [])
+
+  const [initialSplash, setInitialSplash] = useState(false)
+
+  function getConfig(i: number) {
+    return {
+      SPLAT_RADIUS: window.fluidConfig.SPLAT_RADIUS + 0.1 * i,
+      SPLAT_FORCE: window.fluidConfig.SPLAT_FORCE + 2000 * i,
+    };
+  }
+
+
+  useEffect(() => {
+    if (initialSplash) return;
+    setTimeout(() => {
+      let i = 0;
+
+      let interval: NodeJS.Timeout;
+      interval = setInterval(() => {
+        if (i > 1) {
+          clearInterval(interval)
+          return
+        }
+        i++;
+        invokeSplash(getConfig(i))
+      }, 2000);
+    }, 1000)
+    setInitialSplash(true)
+  }, [initialSplash])
+
+
+  const [invokedSplashes, setInvokedSplashes] = useState(0)
+  const [guiStarted, setGuiStarted] = useState(false)
+
+
+function invokeSplash(config: Config = { }) {
+  if (typeof window !== 'undefined') {
+
+    if (typeof window.invokeSplash === 'function') {
+      window.invokeSplash()
+    }
+
+    if (typeof window.updateFluidConfig === 'function') {
+      window.updateFluidConfig(config, guiStarted)
+    }
+  }
+}
+
+  function invokeRandomSplash() {
+    setInvokedSplashes(invokedSplashes + 1)
+    const randomSplashConfig:Config = {
+      // between 0.01 and 1.0
+      SPLAT_RADIUS: Math.random(),
+      // between 0 and 8000
+      SPLAT_FORCE: Math.random() * 8000,
+      // between 0.01 and 4.0
+      DENSITY_DISSIPATION: Math.random() * 4,
+    }
+    invokeSplash(randomSplashConfig)
+  }
+
+  useEffect(() => {
+    if (!guiStarted && invokedSplashes >= 6) {
+      if (typeof window.startGUI === 'function') {
+        window.startGUI()
+        setGuiStarted(true)
+      }
+    }
+  }, [guiStarted, invokedSplashes])
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+      <main
+          className={`flex min-h-screen flex-col items-center justify-between ${inter.className}`}
+      >
+        <div className="relative w-screen h-screen">
+          <canvas id="fluid-canvas"/>
+          <div id={"my-box"}
+              onClick={invokeRandomSplash}
+              className="cursor-pointer z-2 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-1/2 sm:w-1/3 py-16 px-8 border-4 border-white text-4xl box-3d"
+              style={{ transition: '0.2s' }}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            <p>Jørgen</p>
+            <p>Lybeck</p>
+            <p>Hansen</p>
+          </div>
         </div>
-      </div>
+        <div className="z-10 max-w-xl w-full items-center justify-between font-mono text-sm lg:flex">
+          <h1>Hello</h1>
+          <p>I am based in Oslo</p>
+          <p>I am a coder</p>
+          <p>I work at FINN.no</p>
+        </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      </main>
   )
 }
